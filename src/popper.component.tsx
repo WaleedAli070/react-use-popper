bd1a737b5615630a5bd0219bf9259e9bca6253df
WaleedAli070 <waleedali070@gmail.com>
2021-01-07 19:12:21 +0500

    added popper containter component for styling

---
+++ src/popper.component.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import styles from './styles.module.css'
+
+export const PopperContainer: FC = ({ children }) => {
+  return (
+    <div role="tooltip" className={styles.popperContainer}>
+      {children}
+    </div>
+  )
+}
