735e12c79395b18ca655a85cacbe5029f2ce32af
WaleedAli070 <waleedali070@gmail.com>
2021-01-08 15:33:46 +0500

    Updates

---
+++ precache-manifest.a2c40aa5714209f4b94cac644bfddcfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87f804469e1eaf60dc773b650601c23",
+    "url": "./index.html"
+  },
+  {
+    "revision": "118b9f28412df6a92d78",
+    "url": "./static/css/main.4496ca9a.chunk.css"
+  },
+  {
+    "revision": "adb1f6643fa805bec975",
+    "url": "./static/js/2.bac73529.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bac73529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "118b9f28412df6a92d78",
+    "url": "./static/js/main.5fea7c2a.chunk.js"
+  },
+  {
+    "revision": "ac87255226c4065c77ec",
+    "url": "./static/js/runtime-main.b0fbdcec.js"
+  }
+]);
