f89010e80bc037097919fb2cc7566566607e544d
WaleedAli070 <waleedali070@gmail.com>
2021-01-11 13:25:39 +0500

    updated `types` to `interfaces`

--- src/popper.intefaces.ts
+++ src/popper.intefaces.ts
@@ -14,25 +14,32 @@ export interface PopperOptions {
   onClose?: () => void;
 }
 
-export type OpenFunc = (
-  element: ((popper: Popper) => React.ReactNode) | React.ReactNode,
-  options?: PopperOptions
-) => PrivatePopper | undefined;
+export interface OpenFunc {
+  (
+    element: ((popper: Popper) => React.ReactNode) | React.ReactNode,
+    options?: PopperOptions
+  ): PrivatePopper | undefined;
+}
 
-export type CloseFunc = (popperId: string) => void;
+export interface CloseFunc {
+  (popperId: string): void;
+};
 
-export type ShowFunc = (
-  event: React.MouseEvent,
-  content?: ((popper: Popper) => React.ReactNode) | React.ReactNode,
-  options?: PopperOptions
-) => PrivatePopper | undefined;
+export interface ShowFunc {
+  (
+    event: React.MouseEvent,
+    content?: ((popper: Popper) => React.ReactNode) | React.ReactNode,
+    options?: PopperOptions
+  ): PrivatePopper | undefined;
+}
 
-export type ToggleFunc = (
-  event: React.MouseEvent,
-  content?: ((popper: Popper) => React.ReactNode) | React.ReactNode,
-  options?: PopperOptions
-) => void;
-
+export interface ToggleFunc {
+  (
+    event: React.MouseEvent,
+    content?: ((popper: Popper) => React.ReactNode) | React.ReactNode,
+    options?: PopperOptions
+  ): void;
+}
 export interface PopperManager {
   open: OpenFunc;
   close: CloseFunc;
